fd4579e51325c87c6bfe7b32a5c57f7e66c539f0
Joberth Rogers <joberth.rogers18@gmail.com>
2019-09-27 17:11:13 -0400

    Sign up the student with api

--- src/pages/SignUpStudent/index.js
+++ src/pages/SignUpStudent/index.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
 
 import './styles.css';
+import axios from '../../services/api';
 
 export default class SignUpStudent extends Component {
+
+  state = {
+    id: '',
+    name: ''
+  }
+
+  createNotification = (type, message) => {
+  
+    if (type === 'success'){
+      NotificationManager.success( message, 'Sucesso');
+    }
+    else if (type === 'error'){
+      NotificationManager.error(message, 'Erro');
+    }
+
+  };
+
+  handleForm = async event => { 
+    event.preventDefault();
+
+    let response =  await axios.post('/student', {
+      id: this.state.id,
+      name: this.state.name
+    });
+
+    this.createNotification(response.data.type, response.data.msg);
+
+  }
+
   render() {
     return (
       <div className="background">
@@ -10,14 +41,27 @@ export default class SignUpStudent extends Component {
           <p>Cadastrar novo aluno</p>
           <div className="inputs">
             <label htmlFor="id">Identificador</label>
-            <input type="text" name="id" />
+            <input 
+              type="text" 
+              name="id" 
+              value={this.state.id}
+              onChange={event => this.setState({...this.state, id: event.target.value})} 
+              required
+            />
             <label htmlFor="name">Nome</label>
-            <input type="text" name="name"/>
+            <input 
+              type="text" 
+              name="name"
+              value={this.state.name}
+              onChange={event => this.setState({...this.state, name: event.target.value})}
+              required
+            />
           </div>
           <div className="footer">
-            <button>Cadastrar</button>
+            <button type="button" onClick={this.handleForm}>Cadastrar</button>
           </div>
-        </div>     
+        </div> 
+        <NotificationContainer/>    
       </div>
     );
   }
